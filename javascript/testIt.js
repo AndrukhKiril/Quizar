d046ec0175a9704b8ddbaab37d58832c151c19ae
Kiril Andrukh <khandruh@gmail.com>
2023-12-23 00:51:43 +0100

    Fixed comments

--- javascript/testIt.js
+++ javascript/testIt.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD:testIt.js
-import { testCheck } from './testCheck.js';
-
-const doc = 'test-it.json';
-
-=======
 import { testCheck } from './testCheck.js';
 
 const doc = '../test-it.json';
 
->>>>>>> b254ce3 (Improved the file structure):javascript/testIt.js
 testCheck(doc);
