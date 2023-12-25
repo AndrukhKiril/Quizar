ef9a61fb2f50e92ed1e1d063ae9efb3c8302f990
Kiril Andrukh <khandruh@gmail.com>
2023-12-25 16:44:04 +0100

    Fixed answer's selection

--- javascript/testCheck.js
+++ javascript/testCheck.js
@@ -13,26 +13,88 @@ function checkRightAnswers(answersElements, rightAnswersIndex) {
 
     for (let i = 0; i < answersElements.length; i++) {
         if (answersElements[i].classList.contains("answer-selected")) {
-            if (rightAnswersIndex === i) {
-                amountOfPoints++;
-                answersElements[i].classList.add('answer-right');
-            } else {
-                answersElements[i].classList.add('answer-wrong');
+            if (typeof rightAnswersIndex === 'number') {
+                if (i === rightAnswersIndex) {
+                    amountOfPoints++;
+                    answersElements[i].classList.add('answer-right');
+                } else {
+                    answersElements[i].classList.add('answer-wrong');
+                }
+            } 
+            if (typeof rightAnswersIndex === 'object') {
+                for (let j = 0; j < rightAnswersIndex.length; j++) {
+                    if (rightAnswersIndex[j] === i) {
+                        console.log(rightAnswersIndex[j] + ' ' + i);
+                        amountOfPoints += 1;
+                        answersElements[i].classList.add('answer-right');
+                    }
+                }
             }
-
-            answersElements[i].classList.remove('answer-selected');
         } else {
             answersElements[i].classList.remove('answer-one');
             answersElements[i].classList.add('answer-wrong');
         }
+        
+        answersElements[i].classList.remove('answer-selected');
     }
 
-    answersElements[rightAnswersIndex].classList.add("answer-expected");
-
+    if (typeof rightAnswersIndex === 'object') {
+        for (let i = 0; i < rightAnswersIndex.length; i++) {
+            answersElements[rightAnswersIndex[i]].classList.add('answer-expected');
+        }
+    }
+    if (typeof rightAnswersIndex === 'number') {
+        answersElements[rightAnswersIndex].classList.add('answer-expected');
+    }
 }
 
-function moveToQuestion(step) {
-    page += step;
+function moveToQuestion(doc, page) {
+    document.querySelector('.question').innerHTML = '';
+    document.querySelector('.grid-answers').innerHTML = '';
+    document.querySelector('.grid-move-and-submit').innerHTML = '';
+
+    fetch(doc)
+        .then(response => response.json())
+        .then(quizData => {
+            const questions = quizData;
+            const answers = questions.questions[page].answers;
+            const rightAnswersIndex = questions.questions[page].right_answers;
+
+            document.querySelector('.question').insertAdjacentHTML('beforeend', `<h1>${questions.questions[page].question}</h1>`);
+
+            for (let i = 0; i < answers.length; i++) {
+                let answer = `<div class=\"answer-one\"><p>${answers[i]}</p></div>`;
+                document.querySelector('.grid-answers').insertAdjacentHTML('beforeend', answer);
+            }
+
+            let answersElements = document.querySelectorAll('.answer-one');
+
+            for (let i = 0; i < answersElements.length; i++) {
+                answersElements[i].addEventListener('click',
+                    () => selectAnswers(answersElements[i], 'answer-one'));
+            }
+
+            const actions = document.querySelector('.grid-move-and-submit');
+
+                if (page !== 0) {
+                    let prevQuestion = '<button id="previous">Previous question</button>';
+                    actions.insertAdjacentHTML('beforeend', prevQuestion);
+                    document.getElementById('previous').addEventListener('click', () => moveToQuestion(doc, page-1));
+                }
+
+                let submit = '<button id="submit">Submit</button>';
+                actions.insertAdjacentHTML('beforeend', submit)
+                document.getElementById('submit').addEventListener('click', () => checkRightAnswers(answersElements, rightAnswersIndex));
+
+                if (page !== questions.questions.length - 1) {
+                    let nextQuestion = '<button id="next">Next question</button>';
+                    actions.insertAdjacentHTML('beforeend', nextQuestion)
+                    document.getElementById('next').addEventListener('click', () => moveToQuestion(doc, page+1));
+                }
+        })
+        .catch(error => {
+            console.error('An error occurred while fetching or parsing the JSON file:', error);
+        });
 }
 
 function testCheck(doc) {
@@ -44,47 +106,35 @@ function testCheck(doc) {
             const questions = quizData;
 
             let page = 0;
-            const pages = quizData.length;
             const answers = questions.questions[page].answers;
             const rightAnswersIndex = questions.questions[page].right_answers;
 
             document.querySelector('.question').insertAdjacentHTML('beforeend', `<h1>${questions.questions[page].question}</h1>`);
 
-            if (questions.questions[page].type === 'one-right') {
-                for (let i = 0; i < answers.length; i++) {
-                    let answer = `<div class=\"answer-one\"><p>${answers[i]}</p></div>`;
-                    list.insertAdjacentHTML('beforeend', answer);
-                }
-
-                let answersElements = document.querySelectorAll('.answer-one');
-                const rightAnswers = rightAnswersIndex-1;
-                console.log(rightAnswers);
-
-                for (let i = 0; i < answersElements.length; i++) {
-                    answersElements[i].addEventListener('click',
-                        () => selectAnswers(answersElements[i], 'answer-one'));
-                }
-
-                const actions = document.querySelector('.grid-move-and-submit');
-                // if (page !== 0) {
-                    let prevQuestion = '<button id="previous">Previous question</button>';
-                    actions.insertAdjacentHTML('beforeend', prevQuestion);
-                    document.getElementById('previous').addEventListener('click', () => moveToQuestion(-1));
-                // }
-    
-                let submit = '<button id="submit">Submit</button>';
-                actions.insertAdjacentHTML('beforeend', submit)
-                document.getElementById('submit').addEventListener('click', () => checkRightAnswers(answersElements, rightAnswers)); 
-    
-                // if (page === pages.length - 1) {
-                    let nextQuestion = '<button id="next">Next question</button>';
-                    actions.insertAdjacentHTML('beforeend', nextQuestion)
-                    document.getElementById('next').addEventListener('click', () => moveToQuestion(1));
-                // }
-            } else if (questions.questions[page].type === 'more-right') {
-                console.log();
+            for (let i = 0; i < answers.length; i++) {
+                let answer = `<div class=\"answer-one\"><p>${answers[i]}</p></div>`;
+                list.insertAdjacentHTML('beforeend', answer);
             }
+
+            let answersElements = document.querySelectorAll('.answer-one');
+
+            for (let i = 0; i < answersElements.length; i++) {
+                answersElements[i].addEventListener('click',
+                    () => selectAnswers(answersElements[i], 'answer-one'));
             }
+
+            const actions = document.querySelector('.grid-move-and-submit');
+
+            let submit = '<button id="submit">Submit</button>';
+            actions.insertAdjacentHTML('beforeend', submit)
+            document.getElementById('submit').addEventListener('click', () => checkRightAnswers(answersElements, rightAnswersIndex)); 
+            
+            if (page !== questions.questions.length - 1) {
+                let nextQuestion = '<button id="next">Next question</button>';
+                actions.insertAdjacentHTML('beforeend', nextQuestion)
+                document.getElementById('next').addEventListener('click', () => moveToQuestion(doc, 1));
+            }
+            } 
         )
         .catch(error => {
             console.error('An error occurred while fetching or parsing the JSON file:', error);
